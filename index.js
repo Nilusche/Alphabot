b94cb246a655a6ba4a83e2eaa24f51de2dfd2fe1
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-03-22 01:06:46 +0100

    clients

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const { Client, Intents } = require('discord.js');
 require('dotenv').config();
 let token = process.env.TOKEN;
 console.log(process.env.TOKEN);
-const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-});
+const client = new Client();
 const config = require("./config.json");
 const role = require("./role");
 const command = require('./command');
